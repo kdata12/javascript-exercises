f1711ac233b106e9565d99f44120c093a1821b60
kevin <kevinvong0129@gmail.com>
2022-12-12 15:03:13 -0700

    pass all removeFromArray.js tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let toBeRemoved = new Set();
 
+    for (const item of args) {
+        toBeRemoved.add(item)
+    }
+
+    let newArray = array.filter(function (item){
+        if (!toBeRemoved.has(item)) {
+            return item
+        }
+    })
+    return newArray;
 };
 
 // Do not edit below this line
