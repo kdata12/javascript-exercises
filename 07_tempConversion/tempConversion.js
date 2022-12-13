ef200f0117ae51ce8e0a6b32fdf80ae535a9a7de
kevin <kevinvong0129@gmail.com>
2022-12-12 20:55:09 -0700

    pass all tempConversion.spec.js tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahreinheit) {
+  return parseFloat(((tempFahreinheit - 32) * (5/9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  return parseFloat(((tempCelsius * (9/5)) + 32).toFixed(1))
 };
 
 // Do not edit below this line
