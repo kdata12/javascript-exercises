47f5711212c0b7c16aef8c71302f82b052a08303
kevin <kevinvong0129@gmail.com>
2022-12-12 19:50:46 -0700

    pass sumAll tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (typeof(x) != "number" || typeof(y) != "number" || x < 0 || y < 0) {
+        return 'ERROR'
+    };
 
+    if (x > y) {
+        let temp = x;
+        x = y;
+        y = temp;
+    };
+
+    let total = 0;
+    for(let i = x; i <= y; i++) {
+        total += i;
+    }
+    return total
 };
 
 // Do not edit below this line
