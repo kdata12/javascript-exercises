d4dc29eb8aaf4fd5bb33c2a6f073d3a672d53b0e
kevin <kevinvong0129@gmail.com>
2022-12-12 19:58:16 -0700

    pass all leapYears tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // if divisble by 100 and 400 then it is true
+
+    //if divisible by 100 then it is false
+
+    //if divisible by 4 then it is true
+
+    if (year % 100 == 0 && year % 400 == 0) {
+        return true;
+    } else if (year % 100 == 0) {
+        return false;
+    } else if (year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
