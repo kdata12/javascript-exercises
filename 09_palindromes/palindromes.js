96edb1044d4026e54ffdf335575dabab45e02a19
kevin <kevinvong0129@gmail.com>
2022-12-16 11:42:18 -0700

    complete plaindrome.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    //replace punctuation, remove spaces, lower case all
+    let nString = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~/\s/g()]/g,"").toLowerCase();
+    //compare with reverse of string
+    let rString = "";
+    for(let i = nString.length-1; i >= 0; i--) {
+        rString += nString[i];
+    }
+    //return true or false
+    return nString === rString
 };
 
 // Do not edit below this line
