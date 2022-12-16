b84e447a41edceac4adfe971c8f1f07e5681e684
kevin <kevinvong0129@gmail.com>
2022-12-16 12:23:34 -0700

    complete fibonacci, getTheTitles, findTheOldest

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (parseInt(n) < 0) {
+        return "OOPS"
+    }
 
+    if (parseInt(n) === 1) {
+        return 1
+    } else if ( parseInt(n) === 0) {
+        return 0
+    }
+    return fibonacci(n-1) + fibonacci(n-2)
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let sorted = people.sort(function(a , b) {
+        //if they are not dead yet, use current date
+        if (!a.yearOfDeath) {
+            const date = (new Date()).getFullYear();
+            a['yearOfDeath'] = date;
+        } else if (!b.yearOfDeath) {
+            const date = (new Date()).getFullYear();
+            b['yearOfDeath'] = date;
+        }
 
+        const aAge =  a.yearOfDeath - a.yearOfBirth
+        const bAge = b.yearOfDeath - b.yearOfBirth
+
+        return aAge > bAge ? -1 : 1 
+    })
+
+    return sorted[0];
 };
 
 // Do not edit below this line
